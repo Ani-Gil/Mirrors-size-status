63fcb5e7e5e69261131d39e608e70f082797453a
Siwoo Lim <dlatldn426@gmail.com>
2023-09-04 17:07:10 +0900

    Log Update

--- push.sh
+++ push.sh
@@ -2,7 +2,7 @@
 
 NOW=$(date "+Update_%m-%d-%Y")
 du -sh /siwoo.org/Array01/* >> /siwoo.org-system/Mirrors-size-status/$NOW
-cd /siwoo.org-system/Mirros-size-status
+cd /siwoo.org-system/Mirrors-size-status
 git add *
 git commit -m "Log Update"
 git push origin master
