9a8bf0a99a69289902fee388cf54d3b0eee08a64
Siwoo Lim <dlatldn426@gmail.com>
2025-02-01 00:00:01 +0900

    Delete

--- push.sh
+++ push.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 NOW=$(date "+Update_%m-%d-%Y")
-du -sh /siwoo.org/Array01/* >> /siwoo.org-system/Mirrors-size-status/$NOW
+du -sh /siwoo.org/6TB-HDD01/* >> /siwoo.org-system/Mirrors-size-status/$NOW
+du -sh /siwoo.org/6TB-HDD02/* >> /siwoo.org-system/Mirrors-size-status/$NOW
 du -sh /siwoo.org/10TB-HDD01/* >> /siwoo.org-system/Mirrors-size-status/$NOW
 du -sh /siwoo.org/10TB-HDD02/* >> /siwoo.org-system/Mirrors-size-status/$NOW
 cd /siwoo.org-system/Mirrors-size-status
