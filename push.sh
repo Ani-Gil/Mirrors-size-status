fbd048603107f9584a6190164470ca166e19e0fc
Siwoo Lim <dlatldn426@gmail.com>
2025-10-01 00:00:01 +0900

    Delete

--- push.sh
+++ push.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 NOW=$(date "+Update_%m-%d-%Y")
-#du -sh /siwoo.org/10TB-HDD01/* >> /siwoo.org-system/Mirrors-size-status/$NOW
-#du -sh /siwoo.org/10TB-HDD02/* >> /siwoo.org-system/Mirrors-size-status/$NOW
+du -sh /siwoo.org/10TB-HDD01/* >> /siwoo.org-system/Mirrors-size-status/$NOW
+du -sh /siwoo.org/10TB-HDD02/* >> /siwoo.org-system/Mirrors-size-status/$NOW
 cd /siwoo.org-system/Mirrors-size-status
 git add Update*
 git commit -m "Log Update"
